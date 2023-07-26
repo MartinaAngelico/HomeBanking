887406567ca6018c2d1e4488c6b01f0609d8dcb5
MartinaAngelico <85707736+MartinaAngelico@users.noreply.github.com>
2023-07-26 08:50:29 -0300

    Agregar archivos de proyecto.

---
+++ HomeBanking/Models/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace HomeBanking.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if(!context.Clients.Any())
+            {
+                //creamos datos de prueba
+                var clients = new Client[]
+                {
+                    new Client {
+                         FirstName = "Martina",
+                         LastName = "Angelico",
+                         Email = "martiangelico@gmail.com",
+                         Password = "marti1324",
+                    },
+                    new Client {
+                         FirstName = "Francisco",
+                         LastName = "Iriart",
+                         Email = "franiriartb@gmail.com",
+                         Password = "fran1324",
+                    },
+                };
+
+                foreach (var client in clients) 
+                {
+                    context.Clients.Add(client); //le paso el nombre de la variable
+                }
+
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
